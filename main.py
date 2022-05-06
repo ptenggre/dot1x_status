387889bd613dba024a02b54991e2431b65b3ef50
Per Tenggren <per@tenggren.se>
2022-05-06 14:28:24 +0200

    commit!

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello")
+# Import Genie
+from genie import testbed
+import pprint
+
+
+print("asd")
+
+testbed = testbed.load('devices.yaml')
+device = testbed.devices['asw01']
+device.connect()
+output = device.parse('show dot1x all')
+
+pprint.pprint(output)
+
+
+print("Good bye!")
